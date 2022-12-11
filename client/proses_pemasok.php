141d94a5137dc4fd9ca9225a8d2ab59a4ee80f78
ilhamshodiq <ilhambheh@gmail.com>
2022-12-11 18:57:47 +0700

    ngefix proses yang dikacau dafa

--- client/proses_pemasok.php
+++ client/proses_pemasok.php
@@ -4,20 +4,20 @@ include "Client_pemasok.php";
 if ($_POST['aksi']=='tambah')
 {
     $data = array("id_pemasok"=>$_POST['id_pemasok'], "nama_pemasok"=>$_POST['nama_pemasok'], "jenis_kelamin"=>$_POST['jenis_kelamin'], "alamat"=>$_POST['alamat'], "no_telp"=>$_POST['no_telp'],"aksi"=>$_POST['aksi']);
-    $abc->tambah_data($data);
+    $abcd->tambah_data($data);
     header('location:../menu/menu_pemasok.php');
 }
 elseif ($_POST['aksi']=='ubah')
 {
     $data = array("id_pemasok"=>$_POST['id_pemasok'], "nama_pemasok"=>$_POST['nama_pemasok'], "jenis_kelamin"=>$_POST['jenis_kelamin'], "alamat"=>$_POST['alamat'], "no_telp"=>$_POST['no_telp'],"aksi"=>$_POST['aksi']);
-    $abc->ubah_data($data);
+    $abcd->ubah_data($data);
     header('location:../menu/menu_pemasok.php');
 }
 elseif ($_GET['aksi']=='hapus')
 {
     $data = array("id_pemasok"=>$_GET['id_pemasok'], "aksi"=>$_GET['aksi']);
-    $abc->hapus_data($data);
+    $abcd->hapus_data($data);
     header('location:../menu/menu_pemasok.php');
 }
-unset($abc,$data);
+unset($abcd,$data);
 ?>
